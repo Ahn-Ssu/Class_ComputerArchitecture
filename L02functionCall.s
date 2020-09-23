d2ad0cb3691691dfc2adcedc7ee78895de6450de
Ahn-Ssu <21600402@handong.edu>
2020-09-23 22:18:00 +0900

    study for week2

--- L02functionCall.s
+++ L02functionCall.s
@@ -25,10 +25,7 @@ addi	$sp, $sp, 4			# $sp = $sp + 4
 jr		$ra					# jump to $ra
 
 
-
-
-
-C_code:
+# C_code:
 #  n = $a0
 # int fact (int n){
 #     if (n<1) return 1;
@@ -40,14 +37,50 @@ sw		$a0, 0($sp)		#
 sw		$ra, 4($sp)		# 
 
 slti    $t0, $a0, 1
-beq		$t0, $zero, not1	# if $t0 == $zero then not1
+beq		$t0, $zero, not0	# if $t0 == $zero then not0
 
 addi	$v0, $zero, 1			# $v0 = $zero + 1
 addi	$sp, $sp, 8			# $sp = $sp + 8
 jr		$ra					# jump to $ra
 
-not1:
+not0:
 addi	$a0, $a0, -1			# $a0 = $a0 + -1
 jal		factorial				# jump to factorial and save position to $ra
+# 해당 위치의 jal 명령어를 실행할때, ra 값은 이 다음 위치인 54번째 라인이당 
+# 호출한 프로그램으로 되돌아 가는 부분 
+lw		$a0, 0($sp)		# 
+lw		$ra, 4($sp)		# 
+addi	$sp, $sp, 8			# $sp = $sp + 8
+mul	    $v0, $a0, $v0		
+jr		$ra					# jump to $ra
+
+
+
+C_code:
+# x,y = a0, a1
+# i = s0 
+# -----------------------------
+# void strcpy ( char x[], char y[]){
+#     int i;
+#     i = 0;
+#     while ( (x[i]=y[i])!='\0')
+#         i++;
+#     }
+strcpy:
+addi	$sp, $sp, -4			# $sp = $sp + -4
+sw		$s0, 0($sp)		# 
+add		$s0,, $zero, $zero		# $s0, = $zero + $zero
+L1:
+add		$t1, $s0, $a1		# $t1 = $s0 + $a1
+lbu     $t2, 0($t1)    
+add		$t3, $s0, $a0		# $t3 = $s0 + $a0
+sb		$t2, 0($t3)		# 
+beq		$t2, $zero, L2	# if $t2 == $zero then L2
+addi	$s0, $s0, 1			# $s0 = $s0 + 1
+j		L1				# jump to L1
+L2: 
+lw		$s0, 0($sp)		# 
+addi	$sp, $sp, 4			# $sp = $sp + 4
+jr		$ra					# jump to $ra
 
 
