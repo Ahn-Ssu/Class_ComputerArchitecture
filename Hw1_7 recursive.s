38984fe92406b06e80d99d54a0ff7689ac651adb
Ahn-Ssu <21600402@handong.edu>
2020-09-28 20:24:31 +0900

    fixed bug

--- Hw1_7 recursive.s
+++ Hw1_7 recursive.s
@@ -38,32 +38,31 @@ syscall
 
 
 findMax:
-addi	$sp, $sp, -12			# $sp = $sp + -12
-sw		$ra, 8($sp)		# 
-sw		$a1, 4($sp)		# 
-sw		$a0, 0($sp)		# 
-slti  $t0, $a0, 2   # n < 2? -> n이 1일때 
-beq		$t0, $zero, elseCase	# if $t0 == $zero then case1
+addi	$sp, $sp, -8			# $sp = $sp + -8
+sw		$ra, 4($sp)		# 
+sw		$a1, 0($sp)		# 
+# slti  $t0, $a0, 2   # n < 2? -> n이 1일때 
+# beq		$t0, $zero, elseCase	# if $t0 == $zero then case1
+bne		$a1, $zero, elseCase	# if $a0 == $zero then elseCase
 #  n이 1 이하일때 
 lw		$v1, 0($a0)		# integerArray[0]
-addi	$sp, $sp, 12	# $sp = $sp + 12 //  스택 복원 
+addi	$sp, $sp, 8	# $sp = $sp + 12 //  스택 복원 
 jr		$ra					  # jump to $ra
 elseCase:
-# else 문 
 addi	$a1, $a1, -1	# $a1 = $a1 + -1 // N-1
 jal		findMax				# jump to findMax and save position to $ra
-lw		$ra, 8($sp)		# save point
-lw		$a1, 4($sp)		# 
-lw		$a0, 0($sp)		# 
-sll   $t0, $a1, 2   # n-1 번째 index에 접근하기 위한 연산 
+lw		$ra, 4($sp)		# 
+lw		$a1, 0($sp)		# 
+addi	$sp, $sp, 8			# $sp = $sp + 8
+sll     $t0, $a1, 2   # n-1 번째 index에 접근하기 위한 연산 
 add		$t1, $a0, $t0	# $t1 = $a0 + $t0
 addi	$t1, $t1, -4	# $t1 = $t1 + -4 // n-1 완성 
 lw		$t2, 0($t1)		# integerArray[N-1]
-slt   $t3, $v1, $t2 # v = 0 return된 findMax 값(temp), t2 array의 마지막 element  (temp<a[n-1])??
+slt     $t3, $v1, $t2 # v = 0 return된 findMax 값(temp), t2 array의 마지막 element  (temp<a[n-1])??
 bne		$t3, $zero, setEndMax	# if $t3 != $zero then ???
-j		  exitMax				# jump to exitMax
+j		exitMax				# jump to exitMax
 setEndMax:
-add		$v1, $t1, $t2		# $v0 = $t1 + $t2
+add		$v1, $zero, $t2		# $v1 = $t1 + $t2
 exitMax:
 jr		$ra					# jump to $ra
 
