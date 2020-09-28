7e81e936ba532cbac00775dfc146f193ceed9004
Ahn-Ssu <21600402@handong.edu>
2020-09-28 14:53:19 +0900

    주석과 논리 점검

--- Hw1_8.s
+++ Hw1_8.s
@@ -15,11 +15,13 @@ char *strcat(char *dest, const char *src) {
  char *pSrc;
 
 pDest = dest;
-pSrc = src;
 
 while(*pDest !='\0'){
   pDest = pDest +1;
 }
+
+pSrc = src;
+
 while(*pSrc !='\0'){
 *pDest = *pSrc;
 
@@ -35,19 +37,19 @@ while(*pSrc !='\0'){
 
 strcat:
 # add		$t2, $zero, $zero		# $t2 = $tzero+ $zero // i 처럼 이동시킬 변수 
-add		$t0, $a0, $zero		# pDest 시작부터 
+add		$t0, $a0, $zero		# pDest 시작부터  // pDest = dest;
 loop1:
-lbu     $t3, 0($t0) 
-addi	$t0, $t0, 1			# $t0 = $t0 + 1 // byte ++ 
-bne		$t3, $zero, loop1	# if $t3 != $zero then loop1  // *pDest != '\0'
-add		$t1, $a1, $zero		# pSrc 시작부터
+lbu     $t3, 0($t0)         # *pDest
+addi	$t0, $t0, 1			# $t0 = $t0 + 1 // byte ++ :  pDest = pDest +1;
+bne		$t3, $zero, loop1	# if $t3 != $zero then loop1  // while(*pDest !='\0')
+add		$t1, $a1, $zero		# pSrc 시작부터 // pSrc = src;
 loop2:
-lbu     $t3, 0($t1)         
-sb		$t3, 0($t0)		# 
-beq		$t3, $zero, exitStr # if $t3 == $zero then exitStr 
-addi	$t0, $t0, 1			# $t0 = $t0 + 1
-addi	$t1, $t1, 1			# $t0 = $t0 + 1
-j		loop2				# jump to loop2
+lbu     $t3, 0($t1)         # *pSrc
+sb		$t3, 0($t0)		    # *pDest = *pSrc; 
+beq		$t3, $zero, exitStr # if $t3 == $zero then exitStr // *pSrc !='\0'
+addi	$t0, $t0, 1			# $t0 = $t0 + 1 // pDest = pDest +1;
+addi	$t1, $t1, 1			# $t0 = $t0 + 1 // pSrc = pSrc +1 ;
+j		loop2				# jump to loop2 // 탈출 못해서 loop
 exitStr:
 add		$v0, $a0, $zero		# $v0 = $a0 + $zero
 jr		$ra					# jump to $ra
